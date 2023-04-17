43a8a4355915e6936cf7e75f0ad36923bae8605f
kevindotklein <kevindotklein@gmail.com>
2023-04-17 19:14:33 -0300

    hello controller, thymeleaf template

---
+++ src/main/java/com/kevindotklein/springmvc/controllers/HelloController.java
@@ -0,0 +1,17 @@
+package com.kevindotklein.springmvc.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/hello")
+public class HelloController {
+
+    @GetMapping
+    public ModelAndView hello(){
+        ModelAndView mv = new ModelAndView("hello");
+        return mv;
+    }
+}
